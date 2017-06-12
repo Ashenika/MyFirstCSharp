32d7013f15a2edaa370e4e80c63967399a85b382
Asheni Perera <Ashenika@users.noreply.github.com>
2017-06-12 18:51:20 +0530

    Add files via upload

---
+++ MyFirstC/MyFirstC/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace MyFirstC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtans = new System.Windows.Forms.TextBox();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.dou = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtans
+            // 
+            this.txtans.Location = new System.Drawing.Point(29, 32);
+            this.txtans.Name = "txtans";
+            this.txtans.Size = new System.Drawing.Size(212, 20);
+            this.txtans.TabIndex = 0;
+            this.txtans.TextChanged += new System.EventHandler(this.txtans_TextChanged);
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(29, 74);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(44, 29);
+            this.one.TabIndex = 1;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(93, 74);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(44, 29);
+            this.two.TabIndex = 2;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(159, 74);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(44, 29);
+            this.three.TabIndex = 3;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(29, 120);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(44, 29);
+            this.four.TabIndex = 4;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(93, 120);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(44, 29);
+            this.five.TabIndex = 5;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(159, 120);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(44, 29);
+            this.six.TabIndex = 6;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(29, 169);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(44, 29);
+            this.seven.TabIndex = 7;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(93, 169);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(44, 29);
+            this.eight.TabIndex = 8;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(159, 169);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(44, 29);
+            this.nine.TabIndex = 9;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(29, 220);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(44, 29);
+            this.zero.TabIndex = 10;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zone_Click);
+            // 
+            // dou
+            // 
+            this.dou.Location = new System.Drawing.Point(93, 220);
+            this.dou.Name = "dou";
+            this.dou.Size = new System.Drawing.Size(44, 29);
+            this.dou.TabIndex = 11;
+            this.dou.Text = ".";
+            this.dou.UseVisualStyleBackColor = true;
+            this.dou.Click += new System.EventHandler(this.dou_Click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(159, 220);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(44, 29);
+            this.equal.TabIndex = 12;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(228, 74);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(44, 29);
+            this.add.TabIndex = 13;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(228, 120);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(44, 29);
+            this.clear.TabIndex = 14;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.dou);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.txtans);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtans;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button dou;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ MyFirstC/MyFirstC/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + one.Text;
+        }
+
+        
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + two.Text;
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + three.Text;
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + four.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + five.Text;
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + six.Text;
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + seven.Text;
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + eight.Text;
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + nine.Text;
+        }
+
+        private void zone_Click(object sender, EventArgs e)
+        {
+            txtans.Text = txtans.Text + zero.Text;
+        }
+
+        double total1 = 0;
+        double total2 = 0;
+
+        private void dou_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            total2 = total1 + double.Parse(txtans.Text);
+            txtans.Text = total2.ToString();
+            total1 = 0;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            total1 = total1 + double.Parse(txtans.Text);
+            txtans.Clear();
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            txtans.Clear();
+        }
+
+        private void txtans_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
